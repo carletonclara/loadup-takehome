5190331575f1faa74afdb4715dc60c72ad4ce72b
Clara Carleton <claracarleton@Claras-MacBook-Pro.local>
2025-02-03 19:44:46 -0500

    fix paths in feature spec and add new spec to test the dynamic price rendering

--- spec/features/request_form_spec.rb
+++ spec/features/request_form_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 class RequestForm
   include Capybara::DSL
   def visit_form
-    visit('http://127.0.0.1:3000/request_form')
+    visit('/request_form')
   end
 
   def visit_admin
-    visit('http://127.0.0.1:3000/bookings')
+    visit('/bookings')
   end
 end
 
@@ -35,6 +35,7 @@ feature "Visit request form" do
         find('div#animal_type_dog', text: "Dog").click
     end
     fill_in 'Sitting Duration', with: 3
+    expect(page).to have_content("$50")
     click_button 'Submit'
     expect(page).to have_content("Confirm your request")
     click_button 'OK'
@@ -48,7 +49,7 @@ feature "Visit request form" do
     expect(page).to have_content("Archie")
     expect(page).to have_content("dog")
     expect(page).to have_content("3")
-    expect(page).to have_content("50")
+    expect(page).to have_content("$50")
   end
 
   scenario "Submit without content, see form validation errors", :js => true do
@@ -62,5 +63,19 @@ feature "Visit request form" do
     expect(page).to have_content("Please select the type of your animal!")
     expect(page).to have_content("Please select an amount of hours between 2 and 8")
     expect(page).to have_content("Please select a date for your request!")
+  end
+
+  scenario "Dynamic price, see price update with form changes", :js => true do
+    request_form.visit_form
+    within('div.ant-select-selector') do
+        find('span.ant-select-selection-wrap').click
+    end
+    within('div.ant-select-dropdown') do
+        find('div#animal_type_dog', text: "Dog").click
+    end
+    fill_in 'Sitting Duration', with: 3
+    expect(page).to have_content("$50")
+    fill_in 'Sitting Duration', with: 8
+    expect(page).to have_content("$100")
   end
 end
